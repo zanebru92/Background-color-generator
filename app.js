44e1748376cf1b32da96c9f595549d67e556e5c8
Bruno Zanella <zanella.bruno@gmail.com>
2022-05-12 11:33:03 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+let btn = document.getElementById('btn');
+
+let color = document.querySelector('.color');
+
+btn.addEventListener('click', ()=>{
+    let randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);    
+}
